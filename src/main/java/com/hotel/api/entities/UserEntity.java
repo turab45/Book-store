1186ee0d5573efc5349cdbea22e5dfb73b700e13
turab45 <turabbajeer202@gmail.com>
2021-09-28 10:20:28 +0500

    add user entity

---
+++ src/main/java/com/hotel/api/entities/UserEntity.java
@@ -0,0 +1,63 @@
+package com.hotel.api.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private Integer id;
+	@Column(length = 20, nullable = false, unique = true)
+	private String userName;
+	@Column(length = 50, nullable = false, unique = true)
+	private String email;
+	@Column(length = 8)
+	private String password;
+	
+	
+	public UserEntity(Integer id, String userName, String email, String password) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.email = email;
+		this.password = password;
+	}
+	
+	public UserEntity() {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
